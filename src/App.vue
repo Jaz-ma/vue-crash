24cb2fb68cf225c367d81dd4f60788990addaa34
Jaz-ma <potatoorgy3@gmail.com>
2022-05-25 23:51:05 +0100

    ...

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="I use Vim while learning frameworks because i hate myself"/>
 </template>
 
 <script>
